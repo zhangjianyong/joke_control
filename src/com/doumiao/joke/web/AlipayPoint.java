5495335aa0667560049b9c82a326de9074bb25fd
zhangjianyong <tearflyinheaven@hotmail.com>
2015-01-14 13:56:23 +0800

    打集分宝加入重试3次

--- src/com/doumiao/joke/web/AlipayPoint.java
+++ src/com/doumiao/joke/web/AlipayPoint.java
@@ -1,5 +1,6 @@
 package com.doumiao.joke.web;
 
+import java.net.SocketTimeoutException;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -47,54 +48,62 @@ public class AlipayPoint {
 						AccountLogStatus.UNPAY.name(), Plat.ALIPAY.name());
 		for (Map<String, Object> l : logs) {
 			long start = System.currentTimeMillis();
-			try {
-				int id = (int) l.get("id");
-				int memberId = (int) l.get("member_id");
-				String account = (String) l.get("account");
-				Integer wealth = (Integer) l.get("wealth");
-				String sn = (String) l.get("serial_number");
-				String ssn = (String) l.get("sub_serial_number");
+			int tryCount = 0;
+			boolean success = false;
+			while (tryCount < 3 && !success) {
+				try {
+					int id = (int) l.get("id");
+					int memberId = (int) l.get("member_id");
+					String account = (String) l.get("account");
+					Integer wealth = (Integer) l.get("wealth");
+					String sn = (String) l.get("serial_number");
+					String ssn = (String) l.get("sub_serial_number");
 
-				req.setUserSymbol(account);
-				req.setUserSymbolType("ALIPAY_LOGON_ID");
-				req.setPointCount(wealth.longValue());
-				req.setMerchantOrderNo(ssn);
-				req.setMemo("[" + Config.get("system_website_name") + "]积分兑换");
-				req.setOrderTime(new Date());
+					req.setUserSymbol(account);
+					req.setUserSymbolType("ALIPAY_LOGON_ID");
+					req.setPointCount(wealth.longValue());
+					req.setMerchantOrderNo(ssn);
+					req.setMemo("[" + Config.get("system_website_name")
+							+ "]积分兑换");
+					req.setOrderTime(new Date());
 
-				// 如果账号有误,退回积分,更新状态为
-				AccountLog ll = new AccountLog();
-				ll.setMemberId(memberId);
-				ll.setAccount(Account.S2);
-				ll.setWealthType(WealthType.REFUND);
-				ll.setWealth(wealth);
-				ll.setStatus(AccountLogStatus.PAYED);
-				ll.setSerialNumber(sn);
-				ll.setSubSerialNmumber(ssn + "r");
-				ll.setOperator("system");
+					// 如果账号有误,退回积分,更新状态为
+					AccountLog ll = new AccountLog();
+					ll.setMemberId(memberId);
+					ll.setAccount(Account.S2);
+					ll.setWealthType(WealthType.REFUND);
+					ll.setWealth(wealth);
+					ll.setStatus(AccountLogStatus.PAYED);
+					ll.setSerialNumber(sn);
+					ll.setSubSerialNmumber(ssn + "r");
+					ll.setOperator("system");
 
-				AlipayPointOrderAddResponse response = client.execute(req,
-						Config.get("alipay_company_token"));
-				String subCode = response.getSubCode();
-				if (response.isSuccess()
-						|| "isv.out_biz_no_exist".equals(subCode)) {
-					accountService.afterPay(id, wealth, account, memberId);
-				} else if ("isp.no_exist_user".equals(subCode)) {
-					ll.setRemark("第三方账号不存在(" + account + ")");
-					accountService.reject(ll, id);
-				} else if ("isp.cif_card_freeze".equals(subCode)) {
-					ll.setRemark("第三方账号被冻结(" + account + ")");
-					accountService.reject(ll, id);
-				} else if ("isp.budgetcore_invoke_error".equals(subCode)||"aop.invalid-auth-token".equals(subCode)) {
-					log.error("alipay point break");
-					break;
-				}else {
-					log.error(sn+":"+response.getSubCode());
+					AlipayPointOrderAddResponse response = client.execute(req,
+							Config.get("alipay_company_token"));
+					String subCode = response.getSubCode();
+					if (response.isSuccess()
+							|| "isv.out_biz_no_exist".equals(subCode)) {
+						accountService.afterPay(id, wealth, account, memberId);
+					} else if ("isp.no_exist_user".equals(subCode)) {
+						ll.setRemark("第三方账号不存在(" + account + ")");
+						accountService.reject(ll, id);
+					} else if ("isp.cif_card_freeze".equals(subCode)) {
+						ll.setRemark("第三方账号被冻结(" + account + ")");
+						accountService.reject(ll, id);
+					} else if ("isp.budgetcore_invoke_error".equals(subCode)
+							|| "aop.invalid-auth-token".equals(subCode)) {
+						log.error("alipay point break");
+						break;
+					} else {
+						log.error(sn + ":" + response.getSubCode());
+					}
+					success = true;
+					log.debug("point:" + (System.currentTimeMillis() - start));
+				} catch(SocketTimeoutException ste){
+					tryCount++;
+				} catch (Exception e) {
+					log.error(e.getMessage());
 				}
-
-				log.debug("point:" + (System.currentTimeMillis() - start));
-			} catch (Exception e) {
-				log.error(e.getMessage());
 			}
 		}
 	}
